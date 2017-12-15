4abbee52490f665aac6bfedc0c4a161def11d458
John Duncan <john@johnduncanphoto.com>
2017-12-15 17:30:46 +0000

    index html changes made

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
 const app = function () {
-  console.log('function running');
     const url = 'https://anapioficeandfire.com/api/characters/583'
-    makeRequest(url, requestComplete)
+    makeRequest(url, requestComplete);
+    console.log('function running');
+
 }
 
 const requestComplete = function () {
   if ( this.status !== 200) {
-    console.log(this.status);
     return;
   }
 }
@@ -16,6 +16,8 @@ const makeRequest = function(url, callback) {
   request.open('GET', url);
   request.addEventListener('load', callback);
   request.send();
+  console.log('loaded');
+
 }
 
 
