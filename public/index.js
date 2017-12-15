556a6d8a8777c30de4f833bb6501a281f0b8387e
John Duncan <john@johnduncanphoto.com>
2017-12-15 19:16:01 +0000

    can pull back house data

--- public/index.js
+++ public/index.js
@@ -4,23 +4,25 @@ const app = function () {
 
     const url = 'https://www.anapioficeandfire.com/api/houses'
     // const one = 'https://anapioficeandfire.com/api/characters/583'
-    makeRequest(url, requestComplete);
+    const house = JSON.parse(localStorage.getItem('house'));
+    console.log(url);
+    makeRequest(url, house);
     console.log('function running');
 
 }
 
 const requestComplete = function () {
-  if ( this.status !== 200) {
+  if ( this.status !== 200) return;
     var jsonString = this.responseText;
-    var GOTdata = JSON.parse(jsonString);
-    populateHouses(GOTdata);
-  }
+    var houses = JSON.parse(jsonString);
+    populateHouses(houses);
+
 }
 
 const makeRequest = function(url, callback) {
   const request = new XMLHttpRequest();
   request.open('GET', url);
-  request.addEventListener('load', callback);
+  request.addEventListener('load', requestComplete);
   request.send();
   console.log('loaded');
 }
@@ -28,11 +30,12 @@ const makeRequest = function(url, callback) {
 //-----------------------------------------------------------API appending code.
 
 var populateHouses = function(houses) {
-  var houseList = document.getElementById('house-name');
-  houses.forEach(function(beer){
+  var ul = document.getElementById('house-name');
+
+  houses.forEach(function(house){
     var li = document.createElement('li');
-    li.innerText = houses.name;
-    houseList.appendChild(li);
+    li.innerText = house.name;
+    ul.appendChild(li);
   });
 };
 
