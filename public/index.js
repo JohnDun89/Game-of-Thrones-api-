620027ddc56040c70152251aea3b0fd182e6836c
John Duncan <john@johnduncanphoto.com>
2017-12-15 20:42:21 +0000

    format changed to list select

--- public/index.js
+++ public/index.js
@@ -2,22 +2,27 @@
 
 const app = function () {
 
-    const dropdown = document.querySelector('#dropdown');
-    const url = 'https://www.anapioficeandfire.com/api/houses?page=1&pageSize=250'
-    // const url = 'https://www.anapioficeandfire.com/api/houses'
-    // const one = 'https://anapioficeandfire.com/api/characters/583'
-    const house = JSON.parse(localStorage.getItem('house'));
-    console.log(url);
-    makeRequest(url, house);
-    console.log('function running');
+  const dropdown = document.querySelector('#dropdown');
+  const url = 'https://www.anapioficeandfire.com/api/houses?page=1&pageSize=250'
+  // const url = 'https://www.anapioficeandfire.com/api/houses'
+  // const one = 'https://anapioficeandfire.com/api/characters/583'
+  const house = JSON.parse(localStorage.getItem('house'));
+  console.log(url);
+  makeRequest(url, house);
+  console.log('function running');
+
+  // const dropDown = document.getElementById('menu');
+  // dropDown.addEventListener('click', function () {
+  //   console.log('clicked');
+
 
 }
 
 const requestComplete = function () {
   if ( this.status !== 200) return;
-    var jsonString = this.responseText;
-    var houses = JSON.parse(jsonString);
-    populateHouses(houses);
+  var jsonString = this.responseText;
+  var houses = JSON.parse(jsonString);
+  populateHouses(houses);
 
 }
 
@@ -32,18 +37,21 @@ const makeRequest = function(url, callback) {
 //-----------------------------------------------------------API appending code.
 
 var populateHouses = function(houses) {
-  var ul = document.getElementById('house-name');
+  var select = document.getElementById('menu');
 
-  houses.forEach(function(house){
-    var li = document.createElement('li');
-    var liSeat = document.createElement('li');
-    li.innerText = house.name + ', ' + house.region;
-    liSeat.innerText = house.region;
-    ul.appendChild(li);
-    ul.appendChild(liSeat);
+  houses.forEach(function(house, index){
+    const option = document.createElement('option');
+    option.innerText = house.name;
+    option.value = index;
+    select.appendChild(option);
+    // var li = document.createElement('li');
+    // var liSeat = document.createElement('li');
+    // li.innerText = house.name + ', ' + house.region;
+    // liSeat.innerText = house.region;
+    // ul.appendChild(li);
+    // ul.appendChild(liSeat);
   });
 };
-
 
 
 
