342da7987b318c8d3a8029aaf922393e19dd28a2
John Duncan <john@johnduncanphoto.com>
2017-12-16 16:51:47 +0000

    can now access through api link house vassels name

--- public/index.js
+++ public/index.js
@@ -5,11 +5,9 @@ const app = function () {
   const dropdown = document.querySelector('#menu');
 
   const url = 'https://www.anapioficeandfire.com/api/houses?page=1&pageSize=250'
-  // const url = 'https://www.anapioficeandfire.com/api/houses'
-  // const one = 'https://anapioficeandfire.com/api/characters/583'
   const house = JSON.parse(localStorage.getItem('house'));
   // console.log(url);
-  makeRequest(url, house);
+  makeRequest(url, requestComplete);
   // console.log('function running');
 
   dropdown.addEventListener('change', function(){
@@ -29,15 +27,29 @@ const requestComplete = function () {
   var jsonString = this.responseText;
   houses = JSON.parse(jsonString);
   populateHouses(houses);
-
 };
 
 const makeRequest = function(url, callback) {
   const request = new XMLHttpRequest();
   request.open('GET', url);
-  request.addEventListener('load', requestComplete);
+  request.addEventListener('load', callback);
   request.send();
   // console.log('loaded');
+};
+
+const makeSingleRequest = function(url, callback) {
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('load', callback);
+  request.send();
+  console.log('loaded');
+};
+
+const requestSingleComplete = function () {
+  if ( this.status !== 200) return;
+  var jsonString = this.responseText;
+  houses = JSON.parse(jsonString);
+  populateOneHouses(houses);
 };
 
 
@@ -61,44 +73,44 @@ const displayHouseRegion = function (house) {
 };
 
 const displayVassalOf = function (house) {
-  console.log('vassal');
+  // console.log('vassal');
   console.log(house.overlord);
+  const vassalStringy = JSON.stringify(house.overlord);
+  const houseP = JSON.parse(localStorage.getItem('vassal'));
+  // console.log(vassalStringy);
+  makeSingleRequest(house.overlord, requestSingleComplete)
 
-  const vassalOf = document.querySelector('#vassal');
-  const vassalStringy = JSON.stringify(house.overlord.name);
-  vassalOf.innerText = 'Vassal to ' + vassalStringy;
 };
+
+const populateVassal = function (lord) {
+  const vassalSelect = document.querySelector('#vassal');
+  const vassalStringy = JSON.stringify(lord);
+  vassalSelect.innerText = vassalStringy;
+}
 
 const displayHouseWords = function (house) {
   const wordsSelect = document.querySelector('#words');
   const wordsStringy = JSON.stringify(house.words);
   if (wordsStringy ==='""'||'" "') { return;
-
-  }
+  };
   wordsSelect.innerText = wordsStringy;
+};
+
+var populateOneHouses = function(house) {
+  var select = document.getElementById('vassal');
+  vassalName = JSON.stringify(house.name);
+  select.innerText = vassalName;
 };
 
 var populateHouses = function(houses) {
   var select = document.getElementById('menu');
-
   houses.forEach(function(house, index){
     const option = document.createElement('option');
     option.innerText = house.name;
     option.value = index;
     select.appendChild(option);
-    // var li = document.createElement('li');
-    // var liSeat = document.createElement('li');
-    // li.innerText = house.name + ', ' + house.region;
-    // liSeat.innerText = house.region;
-    // ul.appendChild(li);
-    // ul.appendChild(liSeat);
   });
 };
-
-
-
-
-
 //------------------------------------------------------Document Event Listener.
 
 document.addEventListener('DOMContentLoaded', app);
